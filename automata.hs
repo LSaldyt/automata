499f0b2faeaed2d342822b0e782ab5a3d7de02d2
LSaldyt <lucassaldyt@gmail.com>
2019-03-15 17:32:49 -0700

    Adds a command-line version of cellular automata

--- automata.hs
+++ automata.hs
@@ -1,15 +1,26 @@
 -- Lucas Saldyt
--- Wolfram-style cellular automata rules are 4 * 8 bits
+-- Wolfram-style cellular automata rules are 8 bits
 -- 
+import Data.List (find)
+import Data.Maybe
 
 initial = [1]
 
 expand array = [0] ++ array ++ [0]
 
-rule segment = 0
+toBinary 0 = [ 0 ]
+toBinary n = toBinary ( n `quot` 2 ) ++ [ n `rem` 2 ]
 
-applyauto array = [rule segment | 
-                                (a, b, c) <- zip3 (init . init $ expanded) (tail . init $ expanded) (tail . tail $ expanded),
-                                let segment = [a, b, c]] where expanded = expand array
+extract Nothing = 0
+extract (Just (a, b)) = b
 
-main = print $ applyauto . expand $ initial
+create_rule n = rule where rule segment = extract $ find (\(a, b) -> a == segment) (zip space (toBinary n))
+                           space = [[x, y, z] | x <- [0, 1], y <- [0, 1], z <- [0, 1]]
+
+placeholder_rule = create_rule 30
+
+iterate_automata array = [placeholder_rule segment | 
+                                (a, b, c) <- zip3 (init . init $ expanded) (init . tail $ expanded) (tail . tail $ expanded),
+                                let segment = [a, b, c]] where expanded = expand . expand $ array
+
+main = print $ iterate_automata . iterate_automata . expand $ initial
