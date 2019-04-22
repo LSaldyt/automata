d83cfe923e3eb1310a2e4326aef32d6b4366aa65
LSaldyt <lucassaldyt@gmail.com>
2019-04-22 12:58:02 -0700

    Adds beginnings of coloring

--- automata.hs
+++ automata.hs
@@ -9,35 +9,29 @@ import Data.Maybe
 extract Nothing = 0
 extract (Just (a, b)) = b
 
--- s = [0, 1]
--- s = [0..8]
-s = [0, 1]
-
-arity = length s
+space = [0, 1]
+arity = length space
 
 toArity 0 = [0]
 toArity n = toArity ( n `quot` arity ) ++ [ n `rem` arity ]
 
 initial = [1]
+
+build_space s = [[x, y, z] | x <- s, y <- s, z <- s]
+create_rule n = rule where rule segment = extract $ find (\(a, b) -> a == segment) (zip (build_space space) (toArity n))
+
+
 expand array = [0] ++ array ++ [0]
 
-total_space = [[x, y, z] | x <- s, y <- s, z <- s]
--- test_arity = toArity 30
+iterate_automata rule array = [rule segment | 
+                             (a, b, c) <- zip3 (init . init $ row) (init . tail $ row) (tail . tail $ row),
+                             let segment = [a, b, c]] where row = expand . expand $ array
 
-create_rule n = rule 
-    where rule segment = extract $ find (\(a, b) -> a == segment) (zip space (toArity n))
-          space = [[x, y, z] | x <- s, y <- s, z <- s]
+rule = create_rule 30
+max_n = 60
 
-
-placeholder_rule = create_rule 30
-
-iterate_automata array = [placeholder_rule segment | 
-                             (a, b, c) <- zip3 (init . init $ expanded) (init . tail $ expanded) (tail . tail $ expanded),
-                             let segment = [a, b, c]] where expanded = expand . expand $ array
-
-max_n = 20
 loop_it state n = do
-    let next_state = iterate_automata state
+    let next_state = iterate_automata rule state
     print next_state
     if n < max_n
         then
@@ -46,7 +40,6 @@ loop_it state n = do
             return ()
 
 main = do
-    print total_space
     print initial
     print (expand initial)
     loop_it (expand initial) 0
