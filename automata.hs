a49524d8a0934dd801fa7e5d886d105df752af03
LSaldyt <lucassaldyt@gmail.com>
2019-04-22 12:49:59 -0700

    Experimental

--- automata.hs
+++ automata.hs
@@ -4,18 +4,30 @@
 import Data.List (find)
 import Data.Maybe
 
-initial = [1]
-expand array = [0] ++ array ++ [0]
 
-toBinary 0 = [ 0 ]
-toBinary n = toBinary ( n `quot` 2 ) ++ [ n `rem` 2 ]
 
 extract Nothing = 0
 extract (Just (a, b)) = b
 
+-- s = [0, 1]
+-- s = [0..8]
+s = [0, 1]
+
+arity = length s
+
+toArity 0 = [0]
+toArity n = toArity ( n `quot` arity ) ++ [ n `rem` arity ]
+
+initial = [1]
+expand array = [0] ++ array ++ [0]
+
+total_space = [[x, y, z] | x <- s, y <- s, z <- s]
+-- test_arity = toArity 30
+
 create_rule n = rule 
-    where rule segment = extract $ find (\(a, b) -> a == segment) (zip space (toBinary n))
-          space = [[x, y, z] | x <- [0, 1], y <- [0, 1], z <- [0, 1]]
+    where rule segment = extract $ find (\(a, b) -> a == segment) (zip space (toArity n))
+          space = [[x, y, z] | x <- s, y <- s, z <- s]
+
 
 placeholder_rule = create_rule 30
 
@@ -23,7 +35,7 @@ iterate_automata array = [placeholder_rule segment |
                              (a, b, c) <- zip3 (init . init $ expanded) (init . tail $ expanded) (tail . tail $ expanded),
                              let segment = [a, b, c]] where expanded = expand . expand $ array
 
-max_n = 100
+max_n = 20
 loop_it state n = do
     let next_state = iterate_automata state
     print next_state
@@ -33,4 +45,8 @@ loop_it state n = do
         else
             return ()
 
-main = loop_it (expand initial) 0
+main = do
+    print total_space
+    print initial
+    print (expand initial)
+    loop_it (expand initial) 0
